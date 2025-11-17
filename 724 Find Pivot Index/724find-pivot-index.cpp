12e37f2865f583fddf6563228fb42ac7db023ee5
Atyant7 <Atyantsrivastava7@gmail.com>
2025-11-17 18:06:46 +0000

    Code challenge submission for question: 724 Find Pivot Index

---
+++ 724 Find Pivot Index/724find-pivot-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = 0 , lsum = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            total += nums[i];
+        }
+        for(int i = 0 ; i < nums.size() ; i++){
+            int rsum = total - lsum - nums[i];
+            if(rsum == lsum) return i;
+            lsum += nums[i];
+        }
+        return -1;
+    }
+};
