d1c4d7f858e58615e2bebbcf930a455e929b679d
Atyant Srivastava <Atyantsrivastava7@gmail.com>
2026-02-09 19:11:58 +0530

    Time: 0 ms (100.00%) | Memory: 65.2 MB (64.21%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        for(int i = 1 ; i <= n; i++){
-            int px = 0 , xp = 0;
-            for(vector<int>&t : trust){
-                if(t[0] == i) px++;
-                else if(t[1] == i) xp++;
-            }
-            if(px == 0 && xp == n - 1) return i;
+        if(n == 1 && trust.size() == 0) return 1;
+        vector<int> outd(n+1 , 0);
+        vector<int> ind(n+1, 0);
+        for(auto &t : trust){
+            int a = t[0];
+            int b = t[1];
+            outd[a]++;
+            ind[b]++;
+        }
+        for(int i = 0 ; i <= n ; i++){
+            if(outd[i] == 0 && ind[i] == n-1) return i;
         }
         return -1;
     }
