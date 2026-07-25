2a3befff4dc6d095cbe38fd36ff674ca0b229cb7
Atyant Srivastava <Atyantsrivastava7@gmail.com>
2026-07-25 22:24:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (17.86%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -2,7 +2,18 @@ class Solution {
 public:
     int maxProduct(int n) {
         string s = to_string(n);
-        sort(s.begin(), s.end(), greater<int>());
-        return (s[0] - '0') * (s[1] - '0');
+        int m1 = 0;
+        int m2 = 0;
+        for(int i = 0 ; i < s.size() ; i++){
+            if(m1 <= (s[i] - '0')){
+                m2 = m1;
+                m1 = s[i] - '0';
+            }
+            else if(m2 < (s[i] - '0') && m1 > (s[i] - '0')){
+                m2 = s[i] - '0';
+            }
+        }
+        return m1*m2;
+        
     }
 };
