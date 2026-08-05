6c948af26c26373fa57dc1e898f9ed12c442361c
Atyant Srivastava <Atyantsrivastava7@gmail.com>
2026-08-05 11:56:05 +0530

    Time: 27 ms (19.85%) | Memory: 14.5 MB (97.72%) - LeetSync

---
+++ 147-insertion-sort-list/insertion-sort-list.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode* dummy = new ListNode(1000);
+        while(head){
+            ListNode* node = head->next;
+            ListNode* prev = dummy;
+            while(prev->next && prev->next->val < head->val){
+                prev = prev->next;
+            }
+            head->next = prev->next;
+            prev->next = head;
+            head = node;
+        }
+        return dummy->next;
+    }
+};
