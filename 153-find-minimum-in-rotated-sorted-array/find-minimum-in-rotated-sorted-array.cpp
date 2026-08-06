924524debd58c4231aaec579078dc15799e9a2e6
Atyant Srivastava <Atyantsrivastava7@gmail.com>
2026-08-07 00:56:39 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (50.84%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    int helper(vector<int>& n, int start, int end){
-        if( start >= end ) return n[start];
-        if(n[start] <= n[end]){
-            return n[start];
-        }
+    int binarySearch(vector<int>& arr, int start, int end) {
+        if (start > end)
+            return INT_MAX;
+        if (start == end)
+            return arr[start];
         int mid = start + (end - start) / 2;
-        int left = helper(n, start, mid);
-        int right = helper(n, mid+1, end);
+        if (mid > 0 && mid < arr.size() - 1) {
+            if ((arr[mid] < arr[mid - 1]) && (arr[mid] < arr[mid + 1]))
+                return arr[mid];
+            else if ((arr[mid] > arr[mid - 1]) && (arr[mid] > arr[mid + 1])) {
+                return arr[mid + 1];
+            }
+        }
+        int left = binarySearch(arr, start, mid - 1);
+        int right = binarySearch(arr, mid + 1, end);
         return min(left, right);
     }
     int findMin(vector<int>& nums) {
-        if(nums.size() == 1) return nums[0];
-        if(nums.size() == 2) return min(nums[0] , nums[1]);
-        if(nums[0] < nums[nums.size() - 1]) return nums[0];
-        return helper(nums, 0, nums.size() - 1);
+        if (nums.size() == 1)
+            return nums[0];
+        if (nums[0] < nums.back())
+            return nums[0];
+        return binarySearch(nums, 0, nums.size() - 1);
     }
 };
