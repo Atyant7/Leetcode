ada887cdf618a7f5fcf222c71dc51ad625d0726e
Atyant Srivastava <Atyantsrivastava7@gmail.com>
2026-08-07 00:58:36 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (12.18%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& arr, int start, int end) {
-        if (start > end)
-            return INT_MAX;
-        if (start == end)
-            return arr[start];
-        int mid = start + (end - start) / 2;
-        if (mid > 0 && mid < arr.size() - 1) {
-            if ((arr[mid] < arr[mid - 1]) && (arr[mid] < arr[mid + 1]))
-                return arr[mid];
-            else if ((arr[mid] > arr[mid - 1]) && (arr[mid] > arr[mid + 1])) {
-                return arr[mid + 1];
-            }
-        }
-        int left = binarySearch(arr, start, mid - 1);
-        int right = binarySearch(arr, mid + 1, end);
-        return min(left, right);
-    }
+    // int findMin(vector<int>& nums) {
+    //     int low = 0;
+    //     int high = nums.size() - 1;
+
+    //     while (low < high) {
+    //         int mid = low + (high - low) / 2;
+
+    //         if (nums[mid] > nums[high])
+    //             low = mid + 1;
+    //         else
+    //             high = mid;
+    //     }
+
+    //     return nums[low];
+    // }
     int findMin(vector<int>& nums) {
-        if (nums.size() == 1)
-            return nums[0];
-        if (nums[0] < nums.back())
-            return nums[0];
-        return binarySearch(nums, 0, nums.size() - 1);
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        return nums[low];
     }
 };
