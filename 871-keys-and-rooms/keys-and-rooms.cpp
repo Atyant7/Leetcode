4ab54ebc707038d0d67983876b89e837bd5bdc06
Atyant Srivastava <Atyantsrivastava7@gmail.com>
2026-08-14 14:35:08 +0530

    Time: 849 ms (5.14%) | Memory: 227.5 MB (5.53%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
 
+    void dfs(int i, vector<vector<int>> adj, vector<bool>& vis){
+        vis[i] = true;
+        for(int j = 0 ; j < adj[i].size() ; j++){
+            if(vis[adj[i][j]] == false) dfs(adj[i][j], adj, vis);
+        }
+    }
+
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
         vector<bool> visited(n, false);
-        queue<int> q;
+        // queue<int> q;
         visited[0] = true;
-        q.push(0);
-        while(q.size() > 0){
-            int front = q.front();
-            q.pop();
-            for(int i = 0 ; i < rooms[front].size() ; i++){
-                if(!visited[rooms[front][i]]){
-                    visited[rooms[front][i]] = true;
-                    q.push(rooms[front][i]);
-                }
-            }
-        }
+        // q.push(0);
+        // while(q.size() > 0){
+        //     int front = q.front();
+        //     q.pop();
+        //     for(int i = 0 ; i < rooms[front].size() ; i++){
+        //         if(!visited[rooms[front][i]]){
+        //             visited[rooms[front][i]] = true;
+        //             q.push(rooms[front][i]);
+        //         }
+        //     }
+        // }
+        dfs(0, rooms, visited);
         for(int i = 0 ; i < n ; i++){
             if(!visited[i]) return false;
         }
