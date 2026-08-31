67eec008a0a60642cece4256ace07858ea2961ba
Atyant Srivastava <Atyantsrivastava7@gmail.com>
2026-08-31 18:24:37 +0530

    Time: 7 ms (5.63%) | Memory: 12.5 MB (7.08%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> perm;
-        vector<bool> available(n, true);
-        vector<vector<int>> all;
-        dfs(nums, perm, available, all);
-        return all;
-    }
+    int n = nums.size();
+    vector<vector<int>> all;
+    vector<int> perm;
+    vector<bool> available(n, true);
 
-    void dfs(vector<int>& nums, vector<int>& perm, vector<bool>& available, vector<vector<int>>& all){
-        if(perm.size() == nums.size()){
+    queue<pair<vector<int>, vector<bool>>> bfsQ;
+    bfsQ.push({perm, available}); // push at the back of the queue
+
+    // dfs(nums, available, perm, all);
+
+    while (!bfsQ.empty()) {
+        perm = bfsQ.front().first;
+        available = bfsQ.front().second;
+        bfsQ.pop(); // pop from the front of the queue
+
+        if (perm.size() == nums.size()) {
             all.push_back(perm);
         }
-        else{
-            for(int i = 0 ; i < nums.size() ; i++){
-                if(available[i]){
+        else {
+            for (int i = 0; i < nums.size(); i++) {
+                if (available[i]) {
                     perm.push_back(nums[i]);
                     available[i] = false;
 
-                    dfs(nums, perm, available, all);
-
+                    // dfs(nums, available, perm, all);
+                    bfsQ.push({perm, available});
                     available[i] = true;
                     perm.pop_back();
                 }
             }
         }
     }
+
+    return all;
+}
 };
