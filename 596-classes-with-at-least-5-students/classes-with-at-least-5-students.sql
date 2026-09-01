6520fd9b54fb81dd3801253cfbf0289606c1668a
Atyant Srivastava <Atyantsrivastava7@gmail.com>
2026-09-01 19:08:06 +0530

    Time: 350 ms (58.63%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 -- select class from courses where (select count(student) >= 5 from courses group by class) order by class;
 
-select class from (select class, count(student) as strength from courses group by class) as class_wise_strength where strength >= 5 order by class;
+select class from (select class, count(student) as strength from courses group by class having strength >= 5) as class_wise_strength order by class;
