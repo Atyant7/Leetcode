8fa677ac6d184514628d7a322321e735108b6d52
Atyant Srivastava <Atyantsrivastava7@gmail.com>
2026-09-03 15:04:55 +0530

    Time: 6 ms (52.36%) | Memory: 165.9 MB (31.94%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums) {
+        int evmin = INT_MAX, odmin = INT_MAX;
+        int evi = 0, odi = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] % 2 == 0 && nums[i] < evmin){
+                evmin = min(evmin, nums[i]);
+                // evi = i;
+            }
+            else if(nums[i] % 2 != 0 && nums[i] < odmin){
+                odmin = min(odmin, nums[i]);
+                // odi = i;
+            }
+        }
+        if(evmin - odmin < 1 && evmin != INT_MAX && odmin != INT_MAX) return false;
+        return true;
+    }
+};
