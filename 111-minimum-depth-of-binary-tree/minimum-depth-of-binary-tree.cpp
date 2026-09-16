0dc88e627003385e999aaf57a00389d796ede7ae
Atyant Srivastava <Atyantsrivastava7@gmail.com>
2026-09-16 12:10:24 +0530

    Time: 0 ms (100.00%) | Memory: 146.8 MB (50.89%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -6,22 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int dfs(TreeNode* root) {
-        if (root == nullptr)
+    int minDepth(TreeNode* root) {
+        if(root == NULL)
             return 0;
-        if (root->left == nullptr && root->right == nullptr)
-            return 1;
-        if(root->left == nullptr) {
-            return 1 + dfs(root->right);
-        }
-        if(root->right == nullptr) return 1 + dfs(root->left);
-        return 1 + min(dfs(root->left) , dfs(root->right));
+
+        if(root->left == NULL)
+            return minDepth(root->right) + 1;
+
+        if(root->right == NULL)
+            return minDepth(root->left) + 1;
+
+        return min(minDepth(root->left), minDepth(root->right)) + 1;
     }
-    int minDepth(TreeNode* root) { return dfs(root); }
 };
