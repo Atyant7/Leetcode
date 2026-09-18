fe299786fce17dfc8378c1b20210ca0ff8c660b6
Atyant Srivastava <Atyantsrivastava7@gmail.com>
2026-09-18 12:41:26 +0530

    Time: 0 ms (100.00%) | Memory: 34.9 MB (81.82%) - LeetSync

--- 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.cpp
@@ -11,53 +11,20 @@
  */
 class Solution {
 public:
-    int binarySearch(vector<int> &v, int low, int high, int target){
-        if(low > high) return -1;
-        int mid = low + (high - low)/2;
-        if(v[mid] == target) return mid;
-        if(v[mid] < target) return binarySearch(v, mid + 1, high, target);
-        return binarySearch(v, low, mid - 1, target);
-    }
-    int calSum(vector<int> &v, int i){
-        int n = v.size();
-        int sum = 0;
-        for(int j = i; j < n; j++){
-            sum+=v[j];
+    void dfs(TreeNode* root, int &sum){
+        if(root == nullptr){
+            return;
         }
-        return sum;
+        
+        dfs(root->right, sum);
+        sum+=root->val;
+        root->val = sum;
+        dfs(root->left, sum);
     }
     TreeNode* convertBST(TreeNode* root) {
         if(root == nullptr) return root;
-        vector<int> v;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* temp = q.front();
-            q.pop();
-            v.push_back(temp->val);
-            if(temp->left != nullptr){
-                q.push(temp->left);
-            }
-            if(temp->right != nullptr){
-                q.push(temp->right);
-            }
-        }
-        sort(v.begin(), v.end());
-        int n = v.size();
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* temp = q.front();
-            q.pop();
-            int idx = binarySearch(v, 0, n, temp->val);
-            int newVal = calSum(v, idx);
-            temp->val = newVal;
-            if(temp->left != nullptr){
-                q.push(temp->left);
-            }
-            if(temp->right != nullptr){
-                q.push(temp->right);
-            }
-        }
-        return root;
+        int sum = 0;
+        dfs(root, sum);
+        return root;           
     }
 };
