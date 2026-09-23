ca3002d902849a62704e42917c3bb997d71622a1
Atyant Srivastava <Atyantsrivastava7@gmail.com>
2026-09-24 00:20:22 +0530

    Time: 234 ms (6.80%) | Memory: 181.3 MB (5.01%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,26 +2,19 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size();
-
         vector<int> prefix(n);
         vector<int> suffix(n);
-
         // Prefix sum
         prefix[0] = nums[0];
-
         for (int i = 1; i < n; i++) {
             prefix[i] = prefix[i - 1] + nums[i];
         }
-
         // Suffix sum
         suffix[n - 1] = nums[n - 1];
-
         for (int i = n - 2; i >= 0; i--) {
             suffix[i] = suffix[i + 1] + nums[i];
         }
-
         int ans = INT_MAX;
-
         // -----------------------------
         // Suffix only
         // -----------------------------
@@ -30,7 +23,6 @@ public:
                 ans = min(ans, n - j);
             }
         }
-
         // -----------------------------
         // Prefix only
         // -----------------------------
@@ -39,39 +31,30 @@ public:
                 ans = min(ans, i + 1);
             }
         }
-
         // -----------------------------
         // Store suffix sums
         // -----------------------------
         unordered_map<int, int> mp;
-
         for (int j = 0; j < n; j++) {
             // Store the rightmost starting index
             mp[suffix[j]] = j;
         }
-
         // -----------------------------
         // Prefix + Suffix
         // -----------------------------
         for (int i = 0; i < n; i++) {
-
             int remaining = x - prefix[i];
-
             if (mp.find(remaining) != mp.end()) {
-
                 int j = mp[remaining];
-
                 // Prefix ends at i
                 // Suffix starts at j
                 // They must not overlap
                 if (j > i) {
                     int operations = (i + 1) + (n - j);
-
                     ans = min(ans, operations);
                 }
             }
         }
-
         return ans == INT_MAX ? -1 : ans;
     }
 };
